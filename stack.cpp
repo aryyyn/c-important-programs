4cd6645b99e86247962d92f3df0ed107cdbb3a81
Aryan <61741148+aryyyn@users.noreply.github.com>
2023-03-08 18:52:36 +0545

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+   
+    stack<string> dad;
+    
+ 
+    dad.push("michaek");
+    dad.push("dadi");
+    dad.push("momi");
+    
+
+    cout << dad.top();
+
+    return 0;
+}
